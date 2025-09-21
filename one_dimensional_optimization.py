fb6f57c9c2d23a9b92ecc1ac37dd7a878d589264
kristis <kristisappdev@gmail.com>
2025-09-21 10:33:21 +0300

    Get closer to correctly looping the bisection method with systematic work, add some TODO comments

--- one_dimensional_optimization.py
+++ one_dimensional_optimization.py
@@ -26,6 +26,12 @@
 def objective_function(x):
     return (x**2 - 5)**2 / 4
 
+# TODO: Define a variable argument function for printing variables
+def print_variables(xargs):
+    print("xargs[1]:", xargs[1])
+    print("-------------------------------------------------")
+    print()
+
 # print (objective_function(1))
 
 def bisection_method(interval = [0, 10], tolerance_lipschitz_constant = 10**-4):
@@ -35,26 +41,22 @@ def bisection_method(interval = [0, 10], tolerance_lipschitz_constant = 10**-4):
     right_bound = interval[1]
     interval_length = right_bound - left_bound
 
-    x1 = left_bound + interval_length / 4
-    x_middle = (left_bound + right_bound) / 2
-
     print("Before initiating the method")
     print("Interval:", [left_bound, right_bound])
     print("Interval length:", interval_length)
     print("-------------------------------------------------")
 
-    # TODO: Optimize the while loop, so that objective function calls are only done when needed
-    # and the points x1/x_middle/x2 are only calculated when needed (re-assigned otherwise)
-
     iteration = 0
     objective_function_calls = 0
     
     while (interval_length > tolerance_lipschitz_constant):
+        go_to_section_6 = False
+        
         left_bound = interval[0]
         right_bound = interval[1]
         interval_length = right_bound - left_bound
-        x1 = left_bound + interval_length / 4
-        x_middle = (left_bound + right_bound) / 2
+        x1 = left_bound + interval_length / 4 # 1. 
+        x_middle = (left_bound + right_bound) / 2 # 2.
 
         print("Current iteration:", iteration)
         print("Interval:", [left_bound, right_bound])
@@ -63,19 +65,26 @@ def bisection_method(interval = [0, 10], tolerance_lipschitz_constant = 10**-4):
         f_x1 = objective_function(x1)
         f_x_middle = objective_function(x_middle)
 
-        if (f_x1 < f_x_middle):
-            right_bound = x_middle
-        else: 
-            x2 = right_bound - interval_length / 4
-            f_x2 = objective_function(x2)
+        if (f_x1 < f_x_middle): # 3.
+            right_bound = x_middle # 3.1
+            x_middle = x1 # 3.2
+            f_x_middle = f_x1 # (3.2, kind of)
+            go_to_section_6 = True # 3.3
 
-        if (f_x2 < f_x_middle):
+        # else: 
+        # TODO: in all iterations except for first, here x2 is already properly set as well as f_x2
+        #     x2 = right_bound - interval_length / 4
+        #     f_x2 = objective_function(x2)
 
-            left_bound = x_middle
-
+        if go_to_section_6:
+            print_variables(x1)
+            continue
         else:
-            left_bound = x1
-            right_bound = x2
+            if (f_x2 < f_x_middle):
+                left_bound = x_middle
+            else:
+                left_bound = x1
+                right_bound = x2
 
         print("x1:", x1)
         print("x2:", x2)
