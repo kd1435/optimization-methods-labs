45c72be09423f086029f566ef516d0f113e69f36
kristis <kristisappdev@gmail.com>
2025-09-23 12:17:14 +0300

    Move prints to a function print_variables, add type hints almost everywhere possible, complete TODOs, add new TODOs

--- one_dimensional_optimization.py
+++ one_dimensional_optimization.py
@@ -23,35 +23,37 @@
 #     x2 : float
 #     interval_length : list[int]
 
-# TODO: Type hints for function arguments
-def objective_function(x, objective_function_calls = 0):
+def objective_function(x: float, objective_function_calls: int = 0):
     objective_function_calls += 1
     return (x**2 - 5)**2 / 4
 
-# TODO: Define a variable argument function for printing variables
-# def print_variables(xargs):
-#     print("xargs[1]:", xargs[1])
-#     print("-------------------------------------------------")
-#     print()
+def print_variables(**kwargs):
+    for name, value in kwargs.items():
+        print(f"{name}: {value}")
+    print("-------------------------------------------------")
+    print()
 
-# TODO: Type hints for function arguments
-def bisection_method(interval = [0, 10], tolerance_lipschitz_constant = 10**-4):
+def bisection_method(interval: tuple[int, int] = (0, 10), tolerance_lipschitz_constant: float = 10**-4):
     left_bound: float = interval[0]
     right_bound: float = interval[1]
-    interval_length = right_bound - left_bound
+    interval_length: float = right_bound - left_bound
 
     print("Before initiating the method")
     print("Interval:", [left_bound, right_bound])
     print("Interval length:", interval_length)
     print("-------------------------------------------------")
+    print()
 
-    iteration = 0
-    objective_function_calls = 0
+    iteration: int = 0
+    objective_function_calls: int = 0
+    # Do I even need f_min ? 
+    # f_min: float | None = None 
+
+    x_middle: float = (left_bound + right_bound) / 2 # 1.
     x1: float | None = None
-    x_middle: float | None = None
     x2: float | None = None
+    f_x_middle: float = objective_function(x_middle, objective_function_calls) # 1.
     f_x1: float | None = None
-    f_x_middle: float | None = None
     f_x2: float | None = None
     
     while interval_length > tolerance_lipschitz_constant:
@@ -60,28 +62,26 @@ def bisection_method(interval = [0, 10], tolerance_lipschitz_constant = 10**-4):
         print("Current iteration:", iteration)
         print("Interval:", [left_bound, right_bound])
         print("Interval length:", interval_length)
-        
-        if x_middle is None:
-            x_middle = (left_bound + right_bound) / 2 # 1.
-            f_x_middle = objective_function(x_middle, objective_function_calls) # 1.
+        print()
+            
         x1 = left_bound + interval_length / 4 # 2.
         f_x1 = objective_function(x1, objective_function_calls) # 2.
         
         if f_x1 < f_x_middle: # 3. x_middle becomes x_1
+            # f_min = f_x1
             right_bound = x_middle # 3.1
             x_middle = x1 # 3.2
             f_x_middle = f_x1 # 3.2
-
-            # print_variables(objective_function_calls)
-            print("objective_function_calls", objective_function_calls)
-            print("x1:", x1)
-            print("x2:", x2)
-            print("x_middle:", x_middle)
-            print("f_x1:", f_x1)
-            print("f_x2:", f_x2)
-            print("f_x_middle:", f_x_middle)
-            print("-------------------------------------------------")
-            print()
+            print_variables(
+                # f_min=f_min,
+                objective_function_calls=objective_function_calls,
+                x1=x1,
+                x_middle=x_middle,
+                x2=x2,
+                f_x1=f_x1,
+                f_x_middle=f_x_middle,
+                f_x2=f_x2
+            )
             interval_length = right_bound - left_bound
             iteration += 1
             continue # 6. is while loop condition
@@ -90,26 +90,46 @@ def bisection_method(interval = [0, 10], tolerance_lipschitz_constant = 10**-4):
             x2 = right_bound - interval_length / 4 # 2.
             f_x2 = objective_function(x2, objective_function_calls) # 2.
         if f_x2 < f_x_middle: # 4. x_middle becomes x_2
+            # f_min = f_x2
             left_bound = x_middle # 4.1 
             x_middle = x2 # 4.2 
             f_x_middle = x2 # 4.2
         else: # 5. x_middle stays x_middle
+            x_middle = x_middle
             left_bound = x1 # 5.1 
             right_bound = x2 # 5.1
 
-        # print_variables(objective_function_calls)
-        print("objective_function_calls", objective_function_calls)
-        print("x1:", x1)
-        print("x2:", x2)
-        print("x_middle:", x_middle)
-        print("f_x1:", f_x1)
-        print("f_x2:", f_x2)
-        print("f_x_middle:", f_x_middle)
-        print("-------------------------------------------------")
-        print()
+        # print("objective_function_calls", objective_function_calls)
+        # print("x1:", x1)
+        # print("x2:", x2)
+        # print("x_middle:", x_middle)
+        # print("f_x1:", f_x1)
+        # print("f_x2:", f_x2)
+        # print("f_x_middle:", f_x_middle)
+        # print("-------------------------------------------------")
+        # print()
 
+        print_variables(
+            # f_min=f_min,
+            objective_function_calls=objective_function_calls,
+            x1=x1,
+            x_middle=x_middle,
+            x2=x2,
+            f_x1=f_x1,
+            f_x_middle=f_x_middle,
+            f_x2=f_x2
+        )
         interval_length = right_bound - left_bound
         iteration += 1
 
-# TODO: Print objective function?
-bisection_method()
+    # TODO: Plot before and after, or with each operation, the graph of the function, each of the points, interval...
+    # TODO: Print objective function?
+
+# TODO: Golden Section method function definition
+def golden_section_method(interval: tuple[int, int] = (0, 10), tolerance_lipschitz_constant: float = 10**-4):
+    return
+
+# TODO: Newton optimization method function definition
+
+# bisection_method()
+
